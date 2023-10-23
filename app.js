8106e7e8146efaaf79555e97d635c1f2e8037a48
Boris Leighton I <borisleighton@gmail.com>
2023-10-23 16:35:06 -0300

    se agrega una query de crear tabla

--- app.js
+++ app.js
@@ -14,3 +14,20 @@ connection.connect(err=>{
     if(err) return console.error('error:', err.message);
     console.log('Connected to MySQL server');
 })
+
+const createTodoTable = `create table if not exists todos(
+    id int primary key auto_increment,
+    title varchar(255) not null,
+    completed tinyint(1) not null default 0
+);`;
+connection.query(createTodoTable, (err, results, fields)=>{
+    if(err) return console.error(err.message);
+});
+
+
+connection.end(function(err) {
+    if (err) {
+      return console.log('error:' + err.message);
+    }
+    console.log('Close the database connection.');
+  });
