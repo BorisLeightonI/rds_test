711c2710c6db1ff1b16197e6f2ad10f1387edd73
Boris Leighton I <borisleighton@gmail.com>
2023-10-23 16:06:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'database-2.cp5ykmwaxgag.sa-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'admin',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'todoapp',
+});
+
+
+connection.connect(err=>{
+    if(err) return console.error('error:', err.message);
+    console.log('Connected to MySQL server');
+})
