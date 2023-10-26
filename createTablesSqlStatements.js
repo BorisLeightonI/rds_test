a5a7af9b031959fc0b272438cba45b3ffe426465
Boris Leighton I <borisleighton@gmail.com>
2023-10-26 09:58:45 -0300

    Autoincrement

--- createTablesSqlStatements.js
+++ createTablesSqlStatements.js
@@ -2,13 +2,13 @@ const createTables = [
     `CREATE TABLE autor (
         id_autor int(11) NOT NULL,
         nombre varchar(250) NOT NULL UNIQUE,
-        PRIMARY KEY (id_autor) AUTO_INCREMENT,
+        PRIMARY KEY (id_autor) ,
       ) ENGINE=InnoDB;
     );`,
     `CREATE TABLE categoria (
         id_categoria int(11) NOT NULL,
         nombre varchar(250) NOT NULL UNIQUE,
-        PRIMARY KEY (id_categoria) AUTO_INCREMENT,
+        PRIMARY KEY (id_categoria) ,
       ) ENGINE=InnoDB;`,
     `CREATE TABLE libro (
         id_libro int(11) NOT NULL,
@@ -23,10 +23,10 @@ const createTables = [
         PRIMARY KEY (id_libro),
       ) ENGINE=InnoDB;`,
     `CREATE TABLE libro_autor (
-        id_LibroAutor int(11) NOT NULL,
+        id_LibroAutor int(11) NOT NULL AUTO_INCREMENT,
         id_Libro int(11) NOT NULL,
         id_Autor int(11) NOT NULL,
-        PRIMARY KEY (id_LibroAutor) AUTO_INCREMENT,
+        PRIMARY KEY (id_LibroAutor) ,
         CONSTRAINT fk_autor FOREIGN KEY (id_Autor) REFERENCES autor (id_autor) ON UPDATE CASCADE,
         CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE,
       ) ENGINE=InnoDB;`,
@@ -34,7 +34,7 @@ const createTables = [
         id_LibroCategoria int(11) NOT NULL,
         id_Libro int(11) NOT NULL,
         id_Categoria int(11) NOT NULL,
-        PRIMARY KEY (id_LibroCategoria) AUTO_INCREMENT,
+        PRIMARY KEY (id_LibroCategoria),
         CONSTRAINT fk_categoria FOREIGN KEY (id_Categoria) REFERENCES categoria (id_categoria) ON UPDATE CASCADE,
         CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE,
       ) ENGINE=InnoDB;`
