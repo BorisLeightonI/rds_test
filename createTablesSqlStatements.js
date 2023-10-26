9a3917e0deb5c977b3108108a095e483bd4a9e03
Boris Leighton I <borisleighton@gmail.com>
2023-10-26 10:03:57 -0300

    Se eliminan comas de mas

--- createTablesSqlStatements.js
+++ createTablesSqlStatements.js
@@ -7,7 +7,7 @@ const createTables = [
     `CREATE TABLE categoria (
         id_categoria int(11) NOT NULL,
         nombre varchar(250) NOT NULL UNIQUE,
-        PRIMARY KEY (id_categoria) ,
+        PRIMARY KEY (id_categoria)
       ) ENGINE=InnoDB;`,
     `CREATE TABLE libro (
         id_libro int(11) NOT NULL,
@@ -19,7 +19,7 @@ const createTables = [
         descripcionCorta varchar(300),
         descripcionLarga text,
         status varchar(10),
-        PRIMARY KEY (id_libro),
+        PRIMARY KEY (id_libro)
       ) ENGINE=InnoDB;`,
     `CREATE TABLE libro_autor (
         id_LibroAutor int(11) NOT NULL AUTO_INCREMENT,
@@ -27,7 +27,7 @@ const createTables = [
         id_Autor int(11) NOT NULL,
         PRIMARY KEY (id_LibroAutor) ,
         CONSTRAINT fk_autor FOREIGN KEY (id_Autor) REFERENCES autor (id_autor) ON UPDATE CASCADE,
-        CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE,
+        CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE
       ) ENGINE=InnoDB;`,
     `CREATE TABLE libro_categoria (
         id_LibroCategoria int(11) NOT NULL,
@@ -35,7 +35,7 @@ const createTables = [
         id_Categoria int(11) NOT NULL,
         PRIMARY KEY (id_LibroCategoria),
         CONSTRAINT fk_categoria FOREIGN KEY (id_Categoria) REFERENCES categoria (id_categoria) ON UPDATE CASCADE,
-        CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE,
+        CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE
       ) ENGINE=InnoDB;`
     ];
 module.exports = {createTables};
