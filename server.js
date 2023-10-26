79ddf9a0b434b29ccbd556ec8b90515cb3606f19
Boris Leighton I <borisleighton@gmail.com>
2023-10-26 11:24:20 -0300

    Primer método get / en bruto

--- server.js
+++ server.js
@@ -11,4 +11,18 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
+app.get('/', (req, res)=>{
+    const connection = require('./config');
+    connection.query('use bookstore', (err, results, fields)=>{
+        if(err) console.log(err.message);
+        // console.log('results', results);
+        console.log(fields);
+    });
+    connection.query('select * from autor', (err, results, fields)=>{
+        if(err) console.log(err.message);
+        console.log(fields);
+    });
+    res.status(200).json({message: 'ok'});
+});
+
 const server = app.listen(port, ()=>console.log('Servidor escuchando peticiones http en puerto', server.address()))
