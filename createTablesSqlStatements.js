dfe05ee1d1e4183c20cfaba345cee96d41275aba
Boris Leighton I <borisleighton@gmail.com>
2023-10-26 09:50:14 -0300

    Actualizando sql create tables queries

--- createTablesSqlStatements.js
+++ createTablesSqlStatements.js
@@ -2,13 +2,13 @@ const createTables = [
     `CREATE TABLE autor (
         id_autor int(11) NOT NULL,
         nombre varchar(250) NOT NULL UNIQUE,
-        PRIMARY KEY (id_autor) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id_autor) AUTO_INCREMENT,
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
     );`,
     `CREATE TABLE categoria (
         id_categoria int(11) NOT NULL,
         nombre varchar(250) NOT NULL UNIQUE,
-        PRIMARY KEY (id_categoria) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id_categoria) AUTO_INCREMENT,
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;`,
     `CREATE TABLE libro (
         id_libro int(11) NOT NULL,
@@ -20,21 +20,21 @@ const createTables = [
         descripcionCorta varchar(300),
         descripcionLarga text,
         status varchar(10),
-        PRIMARY KEY (id_libro) NOT NULL,
+        PRIMARY KEY (id_libro),
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;`,
     `CREATE TABLE libro_autor (
         id_LibroAutor int(11) NOT NULL,
         id_Libro int(11) NOT NULL,
         id_Autor int(11) NOT NULL,
-        PRIMARY KEY (id_LibroAutor) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id_LibroAutor) AUTO_INCREMENT,
         CONSTRAINT fk_autor FOREIGN KEY (id_Autor) REFERENCES autor (id_autor) ON UPDATE CASCADE,
-        CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE;
+        CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE,
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;`,
     `CREATE TABLE libro_categoria (
         id_LibroCategoria int(11) NOT NULL,
         id_Libro int(11) NOT NULL,
         id_Categoria int(11) NOT NULL,
-        PRIMARY KEY (id_LibroCategoria) NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id_LibroCategoria) AUTO_INCREMENT,
         CONSTRAINT fk_categoria FOREIGN KEY (id_Categoria) REFERENCES categoria (id_categoria) ON UPDATE CASCADE,
         CONSTRAINT fk_libro FOREIGN KEY (id_Libro) REFERENCES libro (id_libro) ON UPDATE CASCADE,
       ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;`
