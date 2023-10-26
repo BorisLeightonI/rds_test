07e8d18a653fc4c7383bd67bf8618819e62f24a9
Boris Leighton I <borisleighton@gmail.com>
2023-10-26 12:39:05 -0300

    faltó and id_

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ app.get('/', (req, res)=>{
         // console.log('results', results);
         console.log(fields);
     });
-    connection.query(`select * from autor where id_autor >= ? and id_ <= ?;`, [min, max], (err, results, fields)=>{
+    connection.query(`select * from autor where id_autor >= ? and id_autor <= ?;`, [min, max], (err, results, fields)=>{
         if(err) console.log(err.message);
         autores = results.map(result => ({...result}));
     });
-    connection.query(`select * from categoria where id_categoria >= ? and id_ <= ?;`, [min, max], (err, results, fields)=>{
+    connection.query(`select * from categoria where id_categoria >= ? and id_categoria <= ?;`, [min, max], (err, results, fields)=>{
         if(err) console.log(err.message);
         categorias = results.map(result => ({...result}));
     });
-    connection.query(`select * from libro where id_libro >= ? and id_ <= ?;`, [min, max], (err, results, fields)=>{
+    connection.query(`select * from libro where id_libro >= ? and id_libro <= ?;`, [min, max], (err, results, fields)=>{
         if(err) console.log(err.message);
         libros = results.map(result => ({...result}));
     });
