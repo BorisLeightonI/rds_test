501b478615507a03aa8dd98751f016860f9e227a
Boris Leighton I <borisleighton@gmail.com>
2023-11-17 15:37:33 -0300

    Se agrega archivo para testear descarga de archivo

---
+++ testing_fetch_file.js
@@ -0,0 +1,32 @@
+// const fetch =  require('node-fetch');
+// import fs from 'node:fs';
+const fs = require('fs');
+
+const file = fs.createWriteStream("tempImage.jpg");
+
+function fetching() {
+    const t0 = performance.now();
+    fetch('https://res.cloudinary.com/dirfklbry/image/upload/v1694015195/foo/275fdc21-c7a8-41ae-93b6-b0eb33ea7bcd.jpg.jpg')
+        .then(res => res.arrayBuffer())
+        // .then(data => data.replaceAll('} ', '},'))
+        .then(buffer =>{
+            file.write(buffer);
+            file.end();
+            console.log('Termina then fetch');
+            const tf = performance.now();
+            console.log('tiempo de descarga:', tf-t0);
+            // jsonString = String(json);
+            // jsonString.
+        })
+        .catch(err => console.error(err))
+        
+
+}
+// const https = require('https');
+
+// const request = https.get("https://github.com/dudeonthehorse/datasets/blob/master/amazon.books.json", function(response) {
+//   response.pipe(file);
+// });
+
+fetching();
+console.log('DESPUES DE FETCHING DATA');
