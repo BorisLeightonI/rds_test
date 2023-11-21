13e3690adf16afc6ebe2e15edce4307444d42e2b
Boris Leighton I <borisleighton@gmail.com>
2023-11-21 09:12:33 -0300

    Se agrega openCV al script de python

--- test_OpenCV_readImg.py
+++ test_OpenCV_readImg.py
@@ -1,8 +1,8 @@
-import sys
+import sys, cv2
 url = 'tempImage.jpg'
-# img = cv2.imread(url)
-print('Dentro de python script')
+img = cv2.imread(url)
+print('***** Dentro de python script *****')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
 sys.stdout.write('Dentro de python stdout')
-# print(img.shape[:2])
+print(img.shape[:2])
