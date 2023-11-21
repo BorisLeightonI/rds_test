3e4075d0bee5f25c263b49fef69a56165c0d98d0
Boris Leighton I <borisleighton@gmail.com>
2023-11-21 15:33:12 -0300

    Se aplica un suavisado blur con ventana de 5x5

--- test_OpenCV_readImg.py
+++ test_OpenCV_readImg.py
@@ -1,11 +1,12 @@
 import sys, cv2
 url = 'tempImage.jpg'
 img = cv2.imread(url)
+blur = cv2.blur(img, (5, 5))
 print('***** Dentro de python script *****')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
 print(img.shape[:])
 print('img shape:',img.shape[:2])
-bordes = cv2.Canny(img, 100, 200)
+bordes = cv2.Canny(blur, 100, 200)
 cv2.imwrite('tempImage_bordes.jpg', bordes)
 sys.stdout.write('Dentro de python stdout')
