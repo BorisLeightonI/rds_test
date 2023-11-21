9632eb64d0206cfe2a6e2019a7305b5b2f21e68f
Boris Leighton I <borisleighton@gmail.com>
2023-11-21 08:21:56 -0300

    Se actualiza el script python sin opencv solo funcionalidad básica

--- test_OpenCV_readImg.py
+++ test_OpenCV_readImg.py
@@ -1,8 +1,8 @@
-import cv2, sys
+import sys
 url = 'tempImage.jpg'
-img = cv2.imread(url)
+# img = cv2.imread(url)
 print('Dentro de python script')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
 sys.stdout.write('Dentro de python stdout')
-print(img.shape[:2])
+# print(img.shape[:2])
