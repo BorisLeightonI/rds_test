a8700179a6f1ce26b5ada14e87bd8356c05f60fc
Boris Leighton I <borisleighton@gmail.com>
2023-12-01 12:50:30 -0300

    Se agrega json.dumps para serializar objetos en python

--- test_OpenCV_readImg.py
+++ test_OpenCV_readImg.py
@@ -1,10 +1,11 @@
-import sys, cv2
+import sys, cv2, json
 url = 'tempImage.jpg'
 img = cv2.imread(url)
 blur = cv2.blur(img, (5, 5))
 print('***** Dentro de python script *****')
 print('First param:'+sys.argv[1]+'#')
 print('Second param:'+sys.argv[2]+'#')
+print('un objeto:'+json.dumps({'clave':'valor'}))
 print(img.shape[:])
 print('img shape:',img.shape[:2])
 bordes = cv2.Canny(blur, 100, 200)
