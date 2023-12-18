4fbd643248e4cd85ab28891a6db1d901fc1e34c6
Boris Leighton I <borisleighton@gmail.com>
2023-12-18 16:54:45 -0300

    Archivo Python en función del tipo de imágen

--- utils/run_AnalizadorFisuras.js
+++ utils/run_AnalizadorFisuras.js
@@ -3,11 +3,13 @@ const { spawnSync } = require('child_process');
 function Analizador(req, res, next) {
     const { folder, tipo_imagen } = req.body;
     let file = '';
-
+    console.log(folder);
     if(tipo_imagen==='normal'){
         file = 'Analizador_Fisuras.py';
+        console.log('Analizar Fisuras');
     }else {
         file = 'Analizador_Fotos_Termicas.py'
+        console.log('Analizar Colores');
     }
 
     const python = spawnSync('python3', [file, folder, 'arg2']);
